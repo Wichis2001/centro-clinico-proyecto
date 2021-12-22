8f6d7583d457205d5cb60a4f20074369e7d6bd5d
Manuel Rojas <manuerjs70@gmail.com>
2021-12-22 12:09:06 -0600

    Creación de nuevas paginas para acceso del admin, mod del index.js

--- Proyecto-Centro-Clinico/api/index.js
+++ Proyecto-Centro-Clinico/api/index.js
@@ -2,13 +2,13 @@ const express = require('express');
 const { reset } = require('nodemon');
 const { dirname } = require('path');
 const app = express();
-const path= require('path');
+const path = require('path');
 const port = process.env.port || 5000;
 
 
 app.use(express.static('public'));
-app.use('/css', express.static(__dirname+'public/css'));
-app.use('/js', express.static(__dirname+'public/js'));
+app.use('/css', express.static(__dirname + 'public/css'));
+app.use('/js', express.static(__dirname + 'public/js'));
 
 app.get('/', (req, res) => {
     res.status(201).sendFile(path.join(__dirname, '../views/index.html'));
@@ -25,17 +25,34 @@ app.get('/secretary/venta', (req, res) => {
 app.get('/secretary/registrar-medico', (req, res) => {
     res.status(201).sendFile(path.join(__dirname, '../views/secretary/registrar-medico.html'));
 });
+
+app.get('/admin', (req, res) => {
+    res.status(201).sendFile(path.join(__dirname, '../views/admin/admin.html'));
+});
+
 app.get('/admin/corte-caja', (req, res) => {
     res.status(201).sendFile(path.join(__dirname, '../views/admin/corte-caja.html'));
 });
 app.get('/admin/crear-examen', (req, res) => {
     res.status(201).sendFile(path.join(__dirname, '../views/admin/crear-examen.html'));
 });
+
+app.get('/admin/control-examenes', (req, res) => {
+    res.status(201).sendFile(path.join(__dirname, '../views/admin/control-examenes.html'));
+});
+
 app.get('/admin/crear-usuario', (req, res) => {
     res.status(201).sendFile(path.join(__dirname, '../views/admin/crear-usuario.html'));
 });
+
+app.get('/admin/control-users', (req, res) => {
+    res.status(201).sendFile(path.join(__dirname, '../views/admin/control-users.html'));
+});
 app.get('/admin/registrar-medico', (req, res) => {
     res.status(201).sendFile(path.join(__dirname, '../views/admin/registro-medicos.html'));
+});
+app.get('/admin/control-medicos', (req, res) => {
+    res.status(201).sendFile(path.join(__dirname, '../views/admin/control-medicos.html'));
 });
 app.get('/admin/reportes', (req, res) => {
     res.status(201).sendFile(path.join(__dirname, '../views/admin/reportes.html'));
